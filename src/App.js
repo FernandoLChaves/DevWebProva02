3e7d2b6a3308e8f2537fe128c6c57a50cb654c85
FERNANDO LAU CHAVES DA SILVA <fernando_lc_silva@estudante.sesisenai.org.br>
2025-06-10 18:54:08 -0300

    Segundo Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
 
 function App() {
   return (
